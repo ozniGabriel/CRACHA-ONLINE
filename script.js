75c4385607552dce5693c29943bf7bce4ea95317
Ozni Gabriel <89555336+ozniGabriel@users.noreply.github.com>
2021-10-28 08:02:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,26 +6,44 @@ const linksSocialMedia = {
   twitter: 'ozni_gabriel'
 }
 
-function changeSocialMediaLinks() {
+/* ESSA FUNÇÃO ADICIONA OS LINKS DAS MINHAS REDES SOCIAIS NAS TAGS (a) DA PÁGINA */
+function changeSocialMediaLinks(links) {
   for (let li of socialMedia.children) {
     const social = li.getAttribute('class')
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    li.children[0].href = `https://${social}.com/${links[social]}`
   }
+  return getGithubProfileInfos(links)
 }
 
-function getGithubProfileInfos() {
-  let url = `https://api.github.com/users/${linksSocialMedia.github}`
+/* ESSA FUNÇÃO CONSOME A API DO GITHUB, QUE RETORNARÁ MEUS DADOS PRA EXIBIR NA PÁGINA */
+function getGithubProfileInfos(links) {
+  let url = `https://api.github.com/users/${links.github}`
   fetch(url)
     .then(response => response.json())
-    .then(json => setInfosOfGithub(json))
+    .then(json => showInfosOfGithub(json))
 }
 
-function setInfosOfGithub(data) {
+/* ESSA FUNÇÃO EXIBI NA PÁGINA AS INFORMAÇÕES QUE A API RETORNOU */
+function showInfosOfGithub(data) {
   userImage.src = data.avatar_url
   login.textContent = data.name
   userLoginGithub.textContent = data.login
   userLink.href = data.html_url
   bio.textContent = data.bio
 }
-changeSocialMediaLinks()
-getGithubProfileInfos()
+
+personalize.addEventListener('click', getNewData)
+
+/* ESSA FUNÇÃO PEGA DO USUARIO NOVOS DADOS PARA PERSONALIZAR A PÁGINA */
+function getNewData() {
+  const newLinksSocialMedia = {
+    github: window.prompt('Digite seu Login do Github'),
+    youtube: window.prompt('Digite seu Login do Youtube'),
+    instagram: window.prompt('Digite seu Login do Instagram'),
+    facebook: window.prompt('Digite seu Login do Facebook'),
+    twitter: window.prompt('Digite seu Login do Twitter')
+  }
+  return changeSocialMediaLinks(newLinksSocialMedia)
+}
+
+changeSocialMediaLinks(linksSocialMedia)
